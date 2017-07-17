a5efda1044087a9be8124965f34df8e6fbc2cd84
spuzzelsnest <jan.mpdesmet@gmail.com>
2017-07-18 01:00:58 +0200

    added original controllers

---
+++ .core/app/Http/Controllers/HomeController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Support\Facades\DB , View, App\Footage, App\Type, Illuminate\Support\Facades\Input;
+
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+
+		$mediaCount = DB::table('footages')
+			->select(DB::Raw('count(0) as cnt, typeId, type, description'))
+			->join('types','footages.typeId','=','types.id')
+			->groupBy('footages.typeId')
+			->get();
+
+		return View::make('index')
+			->with('title' , 'WW2: The Presswar')
+			->with('footages', Footage::join('types','footages.typeId', '=', 'types.id')->get(array('footages.*','types.*')))
+			->with('count', $mediaCount);
+    }
+
+    
+    public function test(){
+    	
+    	$lastUpdated = DB::table('footages')
+    		->select('updated_at')
+    		->orderBy('updated_at', 'desc')
+    		->first();
+    		
+    	
+    	$mediaCount = DB::table('footages')
+			->select(DB::Raw('count(0) as cnt, typeId, type, description'))
+			->join('types','footages.typeId','=','types.id')
+			->groupBy('footages.typeId')
+			->get();
+
+		return View::make('test')
+			->with('title' , 'WW2: The Presswar')
+			->with('lastUpdated',  $lastUpdated)
+			->with('footages', Footage::join('types','footages.typeId', '=', 'types.id')->get(array('footages.*','types.*')))
+			->with('count', $mediaCount);
+    	 
+    	
+    }
+
+}
