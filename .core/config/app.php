e67f24d49e85425e458002f97bf751dbe248625e
spuzzelsnest <jan.mpdesmet@gmail.com>
2017-07-18 02:22:34 +0200

    changed app/config html to HTML

--- .core/config/app.php
+++ .core/config/app.php
@@ -233,7 +233,7 @@ return [
 
 	'Image' => Intervention\Image\Facades\Image::class,
 	'Form' => Collective\Html\FormFacade::class,
-	'Html' => Collective\Html\HtmlFacade::class,
+	'HTML' => Collective\Html\HtmlFacade::class,
 	'Date' => Jenssegers\Date\Date::class,
     ],
 
