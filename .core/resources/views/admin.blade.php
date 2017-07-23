a9451105c07ccb841f4dd0c22f38e98b8a773fb4
spuzzelsnest <jan.mpdesmet@gmail.com>
2017-07-23 20:45:44 +0200

    admin form layout working

--- .core/resources/views/admin.blade.php
+++ .core/resources/views/admin.blade.php
@@ -1,7 +1,6 @@
 @extends ('layouts.admin')
 
 @section('mainbody')
-<script src="//maps.googleapis.com/maps/api/js?key=AIzaSyAF7IPUAyR60tGxGYXPoyNobdtOJJm8cIc&callback=initMap"></script>
 
 <div class="container">
 		<script type="text/javascript">
@@ -47,8 +46,89 @@
 		<div id="mapAdmin"></div>
 	</div>
 		<div class="formLayout">
-{{--open form--}}
+			{!! Form::open(array('route' => 'admin.store', 'files' => true)) !!}
 
-
-
+	   		{{--SELECT MEDIA--}}
+	   			<div class="form-group">
+					@foreach ($types as $type)
+						{!! Form::checkbox('typeId', $type->id) !!}
+						{{$type->description}}
+					@endforeach
+				</div>
+			<div class='col-lg-4'>
+			{{--NAME--}}
+						<div class="form-group">
+							{!! Form::label('name','Name: ', array('class' => 'col-lg-3 control-label')) !!}
+							{!! Form::text('name') !!}
+						</div>
+			{{--SELECT DATE--}}
+						<div class="form-group">
+							{!! Form::label('date','Date: ',  array('class' => 'col-lg-3 control-label')) !!}
+							{!! Form::macro('date', function($name, $default = '1944/06/06', $attrs = array()){
+									$item = '<input type="date" name="'. $name .'" ';
+			 						if ($default) {	$item .= 'value="'. $default .'" ';}
+			 						if (is_array($attrs)){
+			 						 foreach ($attrs as $a => $k)
+			      							$item .= $a .'="'. $k .'" ';
+			  							} $item .= ">";
+			 						  return $item;}); !!}
+			 				{!!Form::date('date')!!}
+						</div>
+			{{--PLACE--}}
+						<div class="form-group">
+								{!! Form::label('place','Place: ', array('class' => 'col-lg-3 control-label')) !!}
+								{!! Form::text('place') !!}
+						</div>
+			{{--COUNTRY--}}
+						<div class="form-group">
+								{!! Form::label('country','Country: ', array('class' => 'col-lg-3 control-label')) !!}
+								{!! Form::text('country') !!}
+						</div>
+			{{--SOURCE--}}
+						<div class="form-group">
+								{!! Form::label('source','Source: ', array('class' => 'col-lg-3 control-label')) !!}
+								{!! Form::text('source') !!}
+						</div>
+			{{--SCHORTDESC--}}
+						<div class="form-group">
+								{!! Form::label('shortdesc','Title: ', array('class' => 'col-lg-3 control-label')) !!}
+								{!! Form::text('shortdesc') !!}
+						</div>
+			{{--INFO--}}
+						<div class="form-group">
+								{!! Form::label('info','Info: ', array('class' => 'col-lg-3 control-label')) !!}
+								{!! Form::textarea('info') !!}
+						</div>
+			{{--REMARKS--}}
+						<div class="form-group">
+								{!! Form::label('remarks','Remarks: ', array('class' => 'col-lg-3 control-label')) !!}
+								{!! Form::text('remarks') !!}
+						</div>
+			{{--LAT--}}
+						<div class="form-group">
+								{!! Form::label('lat','Latitude: ', array('class' => 'col-lg-3 control-label')) !!}
+								{!! Form::text('lat', '', ['id' => 'lat']) !!}
+						</div>
+			{{--LNG--}}
+						<div class="form-group">
+								{!! Form::label('lng','longitude: ', array('class' => 'col-lg-3 control-label')) !!}
+								{!! Form::text('lng', '', ['id' => 'lng']) !!}
+						</div>
+			{{--Published--}}
+						<div class="form-group">
+								{!! Form::label('published', 'Published: ', array('class' => 'col-lg-3 control-label')) !!}
+								{!! Form::checkbox('published')!!}
+						</div>
+			{{--ADD MEDIA--}}
+						<div class="form-group">
+								{!! Form::label('Foto', 'Upload Picture: ', array('class' => 'col-lg-3 control-label')) !!}
+								{!! Form::file('file') !!}
+								<div id='message'>Upload your File...</div>
+						</div>
+						{!! Form::submit('Aanmaken', array('class' => 'btn btn-success')) !!}
+						{!! link_to_route('admin.index', 'Cancel', null, array('class' => 'btn btn-warning')) !!}
+					</div>
+			{!! Form::close() !!}
+	</div>
+</div>
 @endsection
