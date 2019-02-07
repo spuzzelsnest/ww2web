699300643e01bfb77ae2248b64fb01163b440fd7
spuzzelsnest <j.mpdesmet@gmail.com>
2019-02-07 02:22:37 +0100

    footer set

--- .core/resources/views/layouts/footer.blade.php
+++ .core/resources/views/layouts/footer.blade.php
@@ -1,8 +1,5 @@
 <footer>
-   <div align="center">
-   <p STYLE="font-size: 10px;">
-    This Project was started in nov 2007. Many respect goes out to all combat journalists and people in conflict zones.
-   <br>Many thanks also to the many contributers, the journalist , the historians, the locals who helped me along the way. Most of the material is not my property!
-   <br>Coded by: Supzzelsnest
-   </p>
+    This Project was started in nov 2007. Many respect goes out to all combat journalists and people in conflict zones.<br>
+    Many thanks also to the many contributers, the journalist , the historians, the locals who helped me along the way. Most of the material is not my property!<br>
+    Coded by: Supzzelsnest
 </footer>
