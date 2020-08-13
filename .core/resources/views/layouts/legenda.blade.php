6cb24a797807e91a71528e2b442867365c7826a5
spuzzelsnest <j.mpdesmet@gmail.com>
2020-08-14 00:22:06 +0200

    cleanup

--- .core/resources/views/layouts/legenda.blade.php
+++ .core/resources/views/layouts/legenda.blade.php
@@ -3,7 +3,7 @@
 <center>
 	{!! Form::open(array('footages')) !!}
 		@foreach($count as $c)
-			{!! Form::checkbox('type', $c->typeId, true) !!}
+			{!! Form::checkbox('type', $c->typeid, true) !!}
 			<img src="img/{!! $c->type !!}.png">
 			{!! $c->cnt !!}
 			{!! $c->description !!}
