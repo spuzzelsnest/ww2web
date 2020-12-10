e48289eb8b4f62bb2f63599e10d9e2efc0740b4d
spuzzelsnest <j.mpdesmet@gmail.com>
2020-12-10 21:44:13 +0100

    SEO improvments

--- .core/resources/views/layouts/footer.blade.php
+++ .core/resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer>
-    This Project was started in 2007. Much respect goes out to all combat journalists and people in conflict zones.<br>
+	<a href="https://ww2maps.net">This project</a> was started in 2007. Much respect goes out to all combat journalists and people in conflict zones.<br>
     Many thanks also to the community, the journalist , the historians and locals who helped me along the way. This content is only used for historic reference!<br>
-    Coded by: Supzzelsnest 
+    Coded by: Supzzelsnest
 </footer>
