40a7c288a2d8ee7d744b01816e967629e9074b90
spuzzelsnest <j.mpdesmet@gmail.com>
2020-12-12 00:18:30 +0100

    fixed SEO issues

--- .core/resources/views/layouts/footer.blade.php
+++ .core/resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer>
 	<a href="https://ww2maps.net">This project</a> was started in 2007 out of respect to all combat journalists and people in conflict zones.<br>
     Many thanks to the community, the journalist, the historians and locals who helped me along the way.<br>
-    Coded by:<a href="mailto:info@ww2maps.net">info@ww2maps.net</a>
+    Coded by: Spuzzelsnest
 </footer>
--- .core/resources/views/layouts/legenda.blade.php
+++ .core/resources/views/layouts/legenda.blade.php
@@ -3,8 +3,8 @@
         <div>
         @foreach($count as $c)
             {!! Form::checkbox('type', $c->typeid, true) !!}
-            <img src="img/{!! $c->type !!}.png" width="25" height="35">&nbsp;{!! $c->cnt !!}
-            <span class="item-label">{!! $c->description !!}</span>
+            <img src="img/{!! $c->type !!}.png" alt="{!! $c->description !!}" width="25" height="35">&nbsp;{!! $c->cnt !!}
+            <span class="icon-label">{!! $c->description !!}</span>
             <wbr>
         @endforeach
         </div>
