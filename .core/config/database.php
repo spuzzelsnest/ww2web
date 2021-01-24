d6673466f395b7d0c1c05d2b76dd3c2102f2eeb0
spuzzelsnest <j.mpdesmet@gmail.com>
2021-01-24 16:29:06 +0100

    rework to msql

--- .core/config/database.php
+++ .core/config/database.php
@@ -35,11 +35,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'wwmapsnn_'),
             'username' => env('DB_USERNAME', 'wwmapsnn'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', 'Bjarne13!'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
