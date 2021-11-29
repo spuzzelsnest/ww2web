0fd657f404c45c7d7d98beab231309da83ce5114
spuzzelsnest <j.mpdesmet@protonmail.com>
2021-11-29 23:22:34 +0100

    added header

---
+++ .core/resources/views/layouts/header.blade.php
@@ -0,0 +1,1 @@
+<div id='legenda'></div> 
--- .core/resources/views/layouts/legenda.blade.php
+++ .core/resources/views/layouts/legenda.blade.php
@@ -1,12 +1,14 @@
-<div id='legenda'>
-    {!! Form::open(array('footages')) !!}
-        <div>
-        @foreach($count as $c)
-            {!! Form::checkbox('type', $c->typeid, true) !!}
-            <img src="img/{!! $c->type !!}.png" alt="{!! $c->description !!}" width="25" height="35">&nbsp;{!! $c->cnt !!}
-            <span class="icon-label">{!! $c->description !!}</span>
-            <wbr>
-        @endforeach
-        </div>
-    {!! Form::close() !!}
-</div>
+<legenda>
+<div id="mediaCount">
+<center>
+	{!! Form::open(array('footages')) !!}
+		@foreach($count as $c)
+			{!! Form::checkbox('type', $c->typeId, true) !!}
+			<img src="img/{!! $c->type !!}.png">
+			{!! $c->cnt !!}
+			{!! $c->description !!}
+		@endforeach
+	{!! Form::close() !!}
+</center>
+	</div>
+</legenda>
