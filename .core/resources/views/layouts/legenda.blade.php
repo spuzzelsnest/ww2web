670e86edf9cc6348a466c2ca66fe43d5a072ebc1
spuzzelsnest <j.mpdesmet@protonmail.com>
2022-05-16 19:00:37 +0200

    updated changes of icons

--- .core/resources/views/layouts/legenda.blade.php
+++ .core/resources/views/layouts/legenda.blade.php
@@ -3,7 +3,7 @@
         <div>
         @foreach($count as $c)
             {!! Form::checkbox('type', $c->typeid, true) !!}
-            <img src="img/{!! $c->type !!}.png" alt="{!! $c->description !!}" width="25" height="35">&nbsp;{!! $c->cnt !!}
+            <img src="css/images/{!! $c->type !!}.png" alt="{!! $c->description !!}" width="25" height="35">&nbsp;{!! $c->cnt !!}
             <span class="icon-label">{!! $c->description !!}</span>
             <wbr>
         @endforeach
