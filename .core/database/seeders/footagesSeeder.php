dc388a58d4725148bd7a6a24b9e0659b00003c3b
ww2maps.net <ww2maps.net@ww2maps.net>
2022-11-15 21:27:52 +0000

    updated seeders

--- .core/database/seeders/footagesSeeder.php
+++ .core/database/seeders/footagesSeeder.php
@@ -1,8 +1,11 @@
 <?php
 
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class footagesSeeder extends Seeder
+class footageSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,12 +14,12 @@ class footagesSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('footages')->insert(
-				
+        \DB::table('footages')->insert(
+
             array(
                     array(
                         'typeid'      => '1',
-                        'name' 	      => 'duinkerke06',
+                        'name'        => 'duinkerke06',
                         'shortdesc'   => 'Fall Gelb',
                         'info'        => 'After the retreat of the British, the village was heavenly damaged and littered with all what could not be taken over the channel.',
                         'lat'         => '51.04112625',
@@ -25,9 +28,10 @@ class footagesSeeder extends Seeder
                         'country'     => 'France',
                         'date'        => '1940-06-05',
                         'source'      => '',
-                        'remarks'     => '',
+                        'remark'     => '',
                         'published'   => '1'
-                   ),        
-            ));
+                ),
+        ));
+
     }
 }
---
+++ .core/database/seeders/typesSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class typesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::table('types')->insert(
+                array(
+                array(      'type'          => 'Afoto',
+                            'description'       => 'Allied Photo'
+                ),
+                array(      'type'          => 'Xfoto',
+                            'description'       => 'Axis Photo'
+                ),
+                array(      'type'          => 'Avideo',
+                            'description'       => 'Allied Video'
+                 ),
+                array(      'type'          => 'Xvideo',
+                            'description'       => 'Axis Video'
+                ),
+                array(      'type'          => 'Aaudio',
+                            'description'       => 'Allied Audio'
+                ),
+                array(      'type'          => 'Xaudio',
+                            'description'       => 'Axis Audio'
+                ),
+            ));
+    }
+}
