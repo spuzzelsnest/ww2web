e6137c0a486366f26d337bd67118e59a4f98aa4a
dockerC <j.mpdesmet@protonmail.com>
2022-11-20 14:57:36 +0000

    uptdate to source and operation tables

---
+++ .core/database/seeders/sourcesSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class sourcesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /DB::table('sources')->insert(
+		array(
+		array( 'source' 
+
+
+
+
+
+			),
+		     ));
+    }
+}
