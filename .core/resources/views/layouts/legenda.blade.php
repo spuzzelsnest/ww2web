6a89083ab8806a707866e91e07806b08bddb2277
Spuzzelsnest <j.mpdesmet@protonmail.com>
2022-11-20 19:32:02 +0100

    updated legenda

--- .core/resources/views/layouts/legenda.blade.php
+++ .core/resources/views/layouts/legenda.blade.php
@@ -2,7 +2,7 @@
     {!! Form::open(array('footages')) !!}
         <div>
         @foreach($count as $c)
-            {!! Form::checkbox('type', $c->typeid, true) !!}
+            {!! Form::checkbox('type', $c->typeId, true) !!}
             <img src="css/images/{!! $c->type !!}.png" alt="{!! $c->description !!}" width="25" height="35">&nbsp;{!! $c->cnt !!}
             <span class="icon-label">{!! $c->description !!}</span>
             <wbr>
