15747ed1774fd8020d4b49544bea8278c2103346
Spuzzelsnest <j.mpdesmet@protonmail.com>
2022-11-20 16:04:26 +0100

    version php 7.4

--- .core/resources/views/layouts/footer.blade.php
+++ .core/resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer>
     This Project was started in 2007. Much respect goes out to all combat journalists and people in conflict zones.<br>
-    Many thanks also to the community, the journalist , the historians and locals who helped me along the way. This content is only used for historic reference!<br>
-    Coded by: Supzzelsnest 
+    Many thanks to the journalists, the community , the historians and locals who helped me along the way. This content is only used for historic reference!<br>
+    Coded by: Spuzzelsnest
 </footer>
