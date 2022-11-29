108bfdd2492ac0f31a0ba3f9b64a885aa28f1da2
Spuzzelsnest <j.mpdesmet@protonmail.com>
2022-11-29 20:02:42 +0100

    added carbon

--- .core/app/Console/Commands/DatabaseBackUp.php
+++ .core/app/Console/Commands/DatabaseBackUp.php
@@ -28,7 +28,7 @@ class DatabaseBackUp extends Command
      */
     public function handle()
     {
-        $fiename = "backup-" . Carbon::now()->format('Y-m-d') . ".gz";
+        $filename = "backup-" . Carbon::now()->format('Y-m-d') . ".gz";
 
         $command = "mysqldump --user=" . env('DB_USERNAME') ." --password=" . env('DB_PASSWORD') . " --host=" . env('DB_HOST') . " " . env('DB_DATABASE') . "  | gzip > " . storage_path() . "/app/backup/" . $filename;
         
