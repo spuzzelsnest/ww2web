1c1045e3456e6a4e8b55a3bbfb5cb911605a8ff5
Spuzzelsnest <j.mpdesmet@protonmail.com>
2022-12-01 14:54:30 +0100

    disabled image upload

--- .core/resources/views/admin.blade.php
+++ .core/resources/views/admin.blade.php
@@ -72,7 +72,7 @@
                         {{--COUNTRY--}}
                                 {!! Form::label('countryId','Country:', array('class' => 'col-lg-3 control-label')) !!}
                                 {!! Form::select('countryId', 
-                                        $countries->id,
+                                        $countryOptions,
                                         null,
                                          ['class'=>'form-control']) !!}
                         <br>
