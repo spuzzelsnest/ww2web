d1ba24335079cf0c06abc599b760aad0e9c73a2d
Spuzzelsnest <j.mpdesmet@protonmail.com>
2022-12-05 17:24:09 +0100

    updated sqlSeeder

--- .core/database/seeders/SqlSeeder.php
+++ .core/database/seeders/SqlSeeder.php
@@ -14,6 +14,8 @@ class SqlSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $path = base_path() . '/database/seeds/data.sql';
+        $sql = file_get_contents($path);
+        DB::unprepared($sql);
     }
 }
