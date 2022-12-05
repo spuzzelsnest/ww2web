14b93c8b15762cc98da5d265f5083fb9f068c3ae
Spuzzelsnest <j.mpdesmet@protonmail.com>
2022-12-06 00:08:16 +0100

    issues with php artisan server changed app.php

--- .core/bootstrap/app.php
+++ .core/bootstrap/app.php
@@ -12,9 +12,12 @@
 */
 
 $app = new Illuminate\Foundation\Application(
-    realpath(__DIR__.'/../../')
+    realpath(__DIR__ . '/../')
 );
 
+$app->bind('path.public', function (){
+    return base_path() . '/../';
+});
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
