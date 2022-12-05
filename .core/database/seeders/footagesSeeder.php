cc4e0f6f6e5318c3699e3ab195ef595f76f0c032
Spuzzelsnest <j.mpdesmet@protonmail.com>
2022-12-05 18:26:02 +0100

    changed fooragesSeeder

--- .core/database/seeders/footagesSeeder.php
+++ .core/database/seeders/footagesSeeder.php
@@ -14,23 +14,8 @@ class footagesSeeder extends Seeder
      */
     public function run()
     {
-        \DB::table('footages')->insert(
-
-            array(
-                    array(
-                        'typeid'      => '1',
-                        'name'        => 'duinkerke06',
-                        'operationId' => '2',
-                        'info'        => 'After the retreat of the British, the village was heavenly damaged and littered with all what could not be taken over the channel.',
-                        'lat'         => '51.04112625',
-                        'lng'         => '2.40394855',
-                        'place'       => 'Duinkerke',
-                        'countryId'   => '1',
-                        'date'        => '1940-06-05',
-                        'sourceId'    => '1',
-                        'remark'      => '',
-                        'published'   => '1'
-                ),
-        ));
+        $path = base_path() . '/database/seeders/footages.sql';
+        $sql = file_get_contents($path);
+        DB::unprepared($sql);
     }
 }
