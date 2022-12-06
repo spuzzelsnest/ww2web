cc33cd4d5e05e98176cd13f4997e01224ae0c279
Spuzzelsnest <j.mpdesmet@protonmail.com>
2022-12-06 17:38:34 +0100

    update filename

--- .core/app/Http/Controllers/AdminController.php
+++ .core/app/Http/Controllers/AdminController.php
@@ -95,7 +95,7 @@ class AdminController extends Controller{
             $f->published   = $request->input('published');
             $f->save();
 
-            $filename = $f->name.'.jpg';
+            return ($f->name).'.jpg';
 
             $request->image->move(public_path('images'), $filename);
 
