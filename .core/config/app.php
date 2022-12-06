a25b0af3709f35903b9ec1da0d426a8fbf6d755b
Spuzzelsnest <j.mpdesmet@protonmail.com>
2022-12-06 13:09:13 +0100

    update images

--- .core/config/app.php
+++ .core/config/app.php
@@ -231,9 +231,8 @@ return [
         'Validator'     => Illuminate\Support\Facades\Validator::class,
         'View'          => Illuminate\Support\Facades\View::class,
         'Carbon'        => Illuminate\Support\Carbon::class,
-
-	//'Image' => Intervention\Image\Facades\Image::class,
-	//'Form' => Collective\Html\FormFacade::class,
+    
+    //'Form' => Collective\Html\FormFacade::class,
 	//'Html' => Collective\Html\HtmlFacade::class,
 	//'Date' => Jenssegers\Date\Date::class,
     ],
